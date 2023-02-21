67c9d561df9685c1adbaec6f7b71ea2f7d3cb1b4
Uriel Spiridione <urielspiridione135@gmail.com>
2023-02-21 15:14:17 -0300

    Added todo-list feature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,139 @@
+mod todo;
+use std::env;
+use std::io;
+use todo::TodoList;
+
 fn main() {
-    println!("Hello, world!");
+    
+    let args: Vec<String> = env::args().collect();
+    let mut todo_list = TodoList::new();
+
+    todo_list.add_to_list("Some string".to_string());
+    todo_list.add_to_list("Some other string".to_string());
+    todo_list.add_to_list("Some string".to_string());
+    todo_list.add_to_list("Some other string".to_string());
+    todo_list.mark_done(2);
+    todo_list.mark_pending(1);
+
+    match args.len() {
+        // No arguments
+        1 => {
+            println!("Hello No args");
+        }
+        // One argument
+        2 => match args[1].as_str() {
+            "add" => {
+                println!("Enter your new task:");
+                let mut input = String::new();
+                io::stdin()
+                    .read_line(&mut input)
+                    .expect("Please enter a valid text.");
+                if input.trim().chars().count() == 0 {
+                    panic!("You must provide an accepted text");
+                } else {
+                    todo_list.add_to_list(input);
+                }
+            }
+            "remove" => {
+                println!("Enter the number of task to remove:");
+                let mut input = String::new();
+                io::stdin()
+                    .read_line(&mut input)
+                    .expect("Please enter a valid text.");
+                let number:usize = input.trim().parse().expect("Input not a number");
+                if number > todo_list.list.len() {
+                    panic!("{} is not a valid task", number);
+                } else {
+                    todo_list.remove_task(number);
+                }
+            }
+            "done" => {
+                println!("Enter the number of task to mark as done:");
+                let mut input = String::new();
+                io::stdin()
+                    .read_line(&mut input)
+                    .expect("Please enter a valid text.");
+                let number:usize = input.trim().parse().expect("Input not a number");
+                if number > todo_list.list.len() {
+                    panic!("{} is not a valid task", number);
+                } else {
+                    todo_list.mark_done(number);
+                }
+            }
+            "undone" =>  {
+                println!("Enter the number of task to mark as undone:");
+                let mut input = String::new();
+                io::stdin()
+                    .read_line(&mut input)
+                    .expect("Please enter a valid text.");
+                let number:usize = input.trim().parse().expect("Input not a number");
+                if number > todo_list.list.len() {
+                    panic!("{} is not a valid task", number);
+                } else {
+                    todo_list.mark_undone(number);
+                }
+            }
+            "pending" => {
+                println!("Enter the number of task to mark as pending:");
+                let mut input = String::new();
+                io::stdin()
+                    .read_line(&mut input)
+                    .expect("Please enter a valid text.");
+                let number:usize = input.trim().parse().expect("Input not a number");
+                if number > todo_list.list.len() {
+                    panic!("{} is not a valid task", number);
+                } else {
+                    todo_list.mark_pending(number);
+                }
+            }
+            "list" => {
+                todo_list.print();
+            }
+            "list-done" => {
+                todo_list.print_list_done();
+            }
+            "list-undone" => {
+                todo_list.print_list_undone();
+            }
+            "list-pending" => {
+                todo_list.print_list_pending();
+            }
+            "list-all" => {
+                todo_list.print_all();
+            }
+            "order-list" => {
+                todo_list.order_list();
+                println!("New ordered list:");
+                todo_list.print();
+            }
+            _ => {
+                panic!("You must provide an accepted command")
+            }
+        },
+        // One command and one argument
+        3 => match args[1].as_str() {
+            "add" => {
+                todo_list.add_to_list(args[2].to_string());
+            }
+            "remove" => {
+                todo_list.remove_task(args[2].parse().expect("Error converting integer"));
+            }
+            "done" => {
+                todo_list.mark_done(args[2].parse().expect("Error converting integer"));
+            }
+            "undone" => {
+                todo_list.mark_undone(args[2].parse().expect("Error converting integer"));
+            }
+            "pending" => {
+                todo_list.mark_pending(args[2].parse().expect("Error converting integer"));
+            }
+            _ => {
+                panic!("You must provide an accepted command")
+            }
+        },
+        // All other cases
+        _ => {
+            println!("Invalid number of arguments");
+        }
+    }
 }
---
+++ src/todo.rs
@@ -0,0 +1,136 @@
+use std::{fmt};
+
+#[derive(PartialEq, Eq)]
+pub enum TodoItemStatus {
+    Done,
+    Undone,
+    Pending,
+}
+
+pub struct TodoList {
+    pub list: Vec<TodoItem>,
+}
+
+impl TodoList {
+    pub fn new() -> TodoList {
+        TodoList { list: Vec::new() }
+    }
+    pub fn add_to_list(&mut self, name: String) {
+        let todo_item = TodoItem::new(name);
+        self.list.push(todo_item);
+    }
+    pub fn print(&self) {
+        for (index, item) in self.list.iter().enumerate() {
+            println!("{}. [ {:?} ] - {}", index, item.status, item.name)
+        }
+    }
+    pub fn mark_done(&mut self, index: usize) {
+        if self.list[index].status == TodoItemStatus::Undone
+            || self.list[index].status == TodoItemStatus::Pending
+        {
+            self.list[index].status = TodoItemStatus::Done
+        } else {
+            self.list[index].status = TodoItemStatus::Undone
+        }
+    }
+    pub fn mark_undone (&mut self, index:usize){
+        if self.list[index].status == TodoItemStatus::Undone {
+            println!("Task currently undone")
+        } else {
+            self.list[index].status = TodoItemStatus::Undone
+        }
+    }
+    pub fn mark_pending(&mut self, index: usize) {
+        if self.list[index].status == TodoItemStatus::Pending {
+            println!("Task currently pending")
+        } else {
+            self.list[index].status = TodoItemStatus::Pending
+        }
+    }
+    pub fn remove_task(&mut self, index: usize) {
+        self.list.remove(index);
+    }
+    pub fn print_list_done(&self) {
+        println!("DONE TASKS:");
+        for (index, item) in self.list.iter().enumerate() {
+            if item.status == TodoItemStatus::Done {
+                println!("{}. [ {:?} ] - {}", index, item.status, item.name)
+            }
+        }
+    }
+    pub fn print_list_undone(&self) {
+        println!("UNDONE TASKS:");
+        for (index, item) in self.list.iter().enumerate() {
+            if item.status == TodoItemStatus::Undone {
+                println!("{}. [ {:?} ] - {}", index, item.status, item.name)
+            }
+        }
+    }
+    pub fn print_list_pending(&self) {
+        println!("PENDING TASKS:");
+        for (index, item) in self.list.iter().enumerate() {
+            if item.status == TodoItemStatus::Pending {
+                println!("{}. [ {:?} ] - {}", index, item.status, item.name)
+            }
+        }
+    }
+    pub fn print_all(&self) {
+        self.print_list_done();
+        self.print_list_undone();
+        self.print_list_pending();
+    }
+    pub fn order_list(&mut self) {
+        let mut ordered_list: TodoList = TodoList::new();
+        for item in self.get_filtered_items_by_status(TodoItemStatus::Undone) {
+            ordered_list.list.push(TodoItem {
+                name: item.name.to_string(),
+                status: TodoItemStatus::Undone,
+            })
+        }
+        for item in self.get_filtered_items_by_status(TodoItemStatus::Pending) {
+            ordered_list.list.push(TodoItem {
+                name: item.name.to_string(),
+                status: TodoItemStatus::Pending,
+            })
+        }
+        for item in self.get_filtered_items_by_status(TodoItemStatus::Done) {
+            ordered_list.list.push(TodoItem {
+                name: item.name.to_string(),
+                status: TodoItemStatus::Done,
+            })
+        }
+        self.list = ordered_list.list;
+    }
+    fn get_filtered_items_by_status(&self, status: TodoItemStatus) -> Vec<&TodoItem> {
+        let ordered = self
+            .list
+            .iter()
+            .filter(|&item| item.status == status)
+            .collect();
+        ordered
+    }
+}
+
+pub struct TodoItem {
+    name: String,
+    status: TodoItemStatus,
+}
+
+impl TodoItem {
+    fn new(name: String) -> TodoItem {
+        TodoItem {
+            name: name,
+            status: TodoItemStatus::Undone,
+        }
+    }
+}
+
+impl fmt::Debug for TodoItemStatus {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match &self {
+            Self::Done => write!(f, "{}", "✔️ "),
+            Self::Undone => write!(f, "{}", "✖️ "),
+            _ => write!(f, "{}", "➖"),
+        }
+    }
+}
